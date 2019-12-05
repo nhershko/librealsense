c5cd5e0d04e996b8955a59b45f554dda74c0e85b
nhershko <naftali.hershkovitz@intel.com>
2019-12-05 15:59:17 +0200

    place holders for rtsp usage

--- src/ethernet/common/stream_profile.hh
+++ src/ethernet/common/stream_profile.hh
@@ -11,24 +11,19 @@ typedef struct stream_resolution
      int hight; 
 };
 
-
-
 class stream_profile
 {
     public:
-    
-    stream_profile(stream_type_id id, stream_resolution res, int fps)
-    :m_stream_id(id),
-     m_stream_res(res),
-     m_stream_fps(fps)
-    {}
+        stream_profile(stream_type_id id, stream_resolution res, int fps)
+        :m_stream_id(id),
+        m_stream_res(res),
+        m_stream_fps(fps){}
 
-    stream_profile(char* sdp_line);
+        stream_profile(char* sdp_line);
 
     private:
-    
-    stream_type_id m_stream_id;
-    stream_resolution m_stream_res;
-    int m_stream_fps;
+        stream_type_id m_stream_id;
+        stream_resolution m_stream_res;
+        int m_stream_fps;
 
 };
