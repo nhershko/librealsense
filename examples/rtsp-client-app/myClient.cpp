ac56d7d7836a082697bd201904a5b4f31233d9f5
Michal Prager <michal.prager@intel.com>
2019-12-30 11:48:56 +0200

    Fix rtsp client API usage

--- examples/rtsp-client-app/myClient.cpp
+++ examples/rtsp-client-app/myClient.cpp
@@ -8,7 +8,7 @@ int main()
     IcamOERtsp* camOErtspInstance = camOERTSPClient::getRtspClient("rtsp://10.12.145.82:8554/unicast", "myClient");
     std::vector<rs2_video_stream> myProfiles;
     ((camOERTSPClient*)camOErtspInstance)->initFunc();
-    myProfiles = camOErtspInstance->queryProfiles();
+    myProfiles = camOErtspInstance->queryStreams();
     std::cout << "Size = " << myProfiles.size() << "\n";
     for (int i =  0; i < myProfiles.size(); i++)
     {
