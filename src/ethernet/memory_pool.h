b5f832427e4de7bb6f3105d2f643eb333663c0e6
cspungin <chaya.spungin@intel.com>
2020-02-05 14:08:41 +0200

    add mempool

---
+++ src/ethernet/memory_pool.h
@@ -0,0 +1,66 @@
+
+#pragma once
+
+#include <liveMedia.hh>
+#include <queue>
+#include <iostream>
+
+#define POOL_SIZE 1000
+class memory_pool
+{
+
+public:
+    memory_pool()
+    {
+        //alloc memory
+        for (int i = 0; i < POOL_SIZE; i++)
+        {
+            unsigned char *mem = new unsigned char[1280 * 720 * 2]; //TODO:to use OutPacketBuffer::maxSize;
+            //memset(mem,'X',1280 * 720 * 3);
+            pool.push(mem);
+        }
+    }
+
+    unsigned char *getNextMem()
+    {
+        unsigned char *mem = nullptr;
+        if (!pool.empty())
+        {
+            mem = pool.front();
+            pool.pop();
+        }
+        return mem;
+    }
+
+    void returnMem(unsigned char* mem)
+    {
+        if (mem != nullptr)
+        {
+            pool.push(mem);
+        }
+        else
+        {
+            std::cout << "returnMem:invalid mem\n";
+        }
+    }
+
+    ~memory_pool()
+    {
+        for (int i = 0; i < POOL_SIZE; i++)
+        {
+            unsigned char *mem = pool.front();
+            pool.pop();
+            if (mem != nullptr)
+            {
+                delete mem;
+            }
+            else
+            {
+                std::cout << "~memory_pool:invalid mem\n";
+            }
+        }
+    }
+
+private:
+    std::queue<unsigned char*> pool;
+};
