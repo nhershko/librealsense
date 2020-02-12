54d2cfe1e18bd97bf685e72531431221a3746c93
Michal Prager <michal.prager@intel.com>
2020-02-12 15:00:06 +0200

    Use decompressed size

--- src/ethernet/camOESink.cpp
+++ src/ethernet/camOESink.cpp
@@ -87,10 +87,10 @@ void camOESink::afterGettingFrame(unsigned frameSize, unsigned numTruncatedBytes
       {
         return;
       }
-      iCompress->decompressBuffer(fReceiveBuffer+sizeof(rs_frame_header), header->size-sizeof(rs_frame_metadata), fto+sizeof(rs_frame_header));
+      int decompressedSize = iCompress->decompressBuffer(fReceiveBuffer+sizeof(rs_frame_header), header->size-sizeof(rs_frame_metadata), fto+sizeof(rs_frame_header));
       // copy metadata
       memcpy(fto+sizeof(rs_over_ethernet_data_header), fReceiveBuffer+sizeof(rs_over_ethernet_data_header), sizeof(rs_frame_metadata));
-      this->m_rtp_callback->on_frame((u_int8_t*)fto+sizeof(rs_over_ethernet_data_header), fstream.width * fstream.height *fstream.bpp + sizeof(rs_frame_metadata), presentationTime);//todo: change to bpp
+      this->m_rtp_callback->on_frame((u_int8_t*)fto+sizeof(rs_over_ethernet_data_header), decompressedSize + sizeof(rs_frame_metadata), presentationTime);//todo: change to bpp
       memPool->returnMem(fReceiveBuffer);
 #else
     this->m_rtp_callback->on_frame(fReceiveBuffer+sizeof(rs_over_ethernet_data_header), header->size, presentationTime);
